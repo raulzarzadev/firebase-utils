67109ad45cf13fbced22d64b92c989f2db1e76ad
zasara <raulzarza@gmail.com>
2022-06-21 14:31:22 -0600

    use dist as build location

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// import { getUser } from './users'
+Object.defineProperty(exports, "__esModule", { value: true });
+class FirebaseClass {
+    static init(params) {
+        console.log('comenzando');
+        console.log('params', params);
+    }
+}
+module.exports = { FirebaseClass };
